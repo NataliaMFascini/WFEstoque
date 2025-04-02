6c45a950832475bcf7a735eb35cc2da815e333d2
nataliamfascini <natalia.mfascini@senacsp.edu.br>
2025-04-02 14:45:26 -0300

    feat: Configurado tela de menu

--- WFCadastroProduto/FormMenu.Designer.cs
+++ WFCadastroProduto/FormMenu.Designer.cs
@@ -34,7 +34,10 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(373, 277);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "FormMenu";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormMenu";
