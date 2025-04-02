c4b79323a81bf886c67ff14ef37a6cab7ce70748
nataliamfascini <natalia.mfascini@senacsp.edu.br>
2025-04-02 14:43:35 -0300

    Add project files.

---
+++ WFCadastroProduto/FormLogin.cs
@@ -0,0 +1,50 @@
+namespace WFCadastroProduto
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            Usuario user = new Usuario();
+            user.id = 1;
+            user.login = "admin";
+            user.senha = "123456";
+            user.nome = "Administrador do Sistema";
+            user.dataCadastro = DateTime.Now;
+
+            Usuario.ListaUsuarios.Add(user);
+
+            user = new Usuario();
+            user.id = 2;
+            user.login = "user";
+            user.senha = "123456789";
+            user.nome = "Usuário do Sistema";
+            user.dataCadastro = DateTime.Now;
+
+            Usuario.ListaUsuarios.Add(user);
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            foreach (Usuario us in Usuario.ListaUsuarios)
+            {
+                if(us.login == txtLogin.Text && us.senha == txtSenha.Text)
+                {
+                    FormMenu form = new FormMenu();
+                    form.ShowDialog();
+                    return;
+                }
+            }
+            Erro("Usuário não autenticado.");
+        }
+
+        private void Erro(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ WFCadastroProduto/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFCadastroProduto
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string? nome { get; set; } //"?" permite o valor nulo
+        public string? login {  get; set; }
+        public string? senha { get; set; }
+        public DateTime dataCadastro { get; set; }
+
+        public static List<Usuario>
+            ListaUsuarios = new List<Usuario>();
+    }
+}
